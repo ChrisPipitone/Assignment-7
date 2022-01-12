e7210a0199cb20184d397927e1117c560a26ed7e
christopher pipitone <Chrispipitone123@gmail.com>
2022-01-11 23:38:22 -0500

    new file:   .gitignore 	new file:   README.md 	new file:   api-key.txt 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   public/manifest.json 	new file:   src/App.js 	new file:   src/components/GifCard.js 	new file:   src/components/SearchField.js 	new file:   src/index.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GifCard from "./components/GifCard";
+import SearchField from "./components/SearchField";
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
